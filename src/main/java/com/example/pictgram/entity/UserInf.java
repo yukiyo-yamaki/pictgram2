f950de0ce3a767e2a707ead3ca9fd3939ac24512
yukiyo <yukiyoyamaki1@gmail.com>
2023-11-06 16:59:26 +0900

    テキスト25

---
+++ src/main/java/com/example/pictgram/entity/UserInf.java
@@ -0,0 +1,9 @@
+package com.example.pictgram.entity;
+
+public interface UserInf {
+	
+	Long getUserId();
+	
+	String getUsername();
+
+}
---
+++ src/main/java/com/example/pictgram/filter/FormAuthenticationProvider.java
@@ -0,0 +1,61 @@
+package com.example.pictgram.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.example.pictgram.entity.User;
+import com.example.pictgram.repository.UserRepository;
+
+@Configuration
+
+public class FormAuthenticationProvider implements AuthenticationProvider {
+	
+	protected static Logger log = LoggerFactory.getLogger(FormAuthenticationProvider.class);
+	
+	@Autowired
+	private UserRepository repository;
+	
+	@Autowired
+	@Lazy
+	private PasswordEncoder passwordEncoder;
+	
+	@Override
+	public Authentication authenticate(Authentication auth) throws AuthenticationException {
+		String name = auth.getName();
+		String password = auth.getCredentials().toString();
+		
+		log.debug("name={}", name);
+		log.debug("password={}", password);
+		
+		if ("".equals(name) || "".equals(password)) {
+			throw new AuthenticationCredentialsNotFoundException("ログイン情報に不備があります。");
+		}
+		
+		User entity = repository.findByUsername(name);
+		if (entity == null) {
+			throw new AuthenticationCredentialsNotFoundException("ログイン情報が存在しません。");
+		}
+		
+		// 追加　入力されたパスワードとデータベースのパスワードを比較
+		if (!passwordEncoder.matches(password, entity.getPassword())) {
+			throw new AuthenticationCredentialsNotFoundException("ログイン情報に不備があります。");
+		}
+		
+		return new UsernamePasswordAuthenticationToken(entity, password, entity.getAuthorities());
+				}
+	
+	@Override
+	public boolean supports(Class<?> authentication) {
+		return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication);
+	}
+
+}
