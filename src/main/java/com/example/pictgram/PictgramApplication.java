e134ee25b421b5b19d8839af3f4d62cfdf934663
yukiyo <yukiyoyamaki1@gmail.com>
2023-11-15 02:47:37 +0900

    テキスト35

--- src/main/java/com/example/pictgram/PictgramApplication.java
+++ src/main/java/com/example/pictgram/PictgramApplication.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class PictgramApplication extends SpringBootServletInitializer {
@@ -15,6 +20,12 @@ public class PictgramApplication extends SpringBootServletInitializer {
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(PictgramApplication.class);
+	}
+	
+	@Bean
+	public WebClient rest(ClientRegistrationRepository clients, OAuth2AuthorizedClientRepository authz) {
+		ServletOAuth2AuthorizedClientExchangeFilterFunction oauth2 = new ServletOAuth2AuthorizedClientExchangeFilterFunction(clients, authz);
+		return WebClient.builder().filter(oauth2).build();
 	}
 
 }
